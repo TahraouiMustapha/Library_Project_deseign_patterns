fcb2a4286850978c4f07f29a80d85b16f565477f
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-04-25 00:24:50 +0100

    feat:  implement the factory method on user implementation, change the user form interface to abstract class

--- src/main/java/myLibrary/Main.java
+++ src/main/java/myLibrary/Main.java
@@ -2,6 +2,11 @@ package main.java.myLibrary;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi ");
+        UserCreator adminFactory = new AdminCreator(); 
+        UserCreator readerFactory = new ReaderCreator();
+        
+        User akram = adminFactory.createUser(0, "akram" , "ahmad@gmail.com");
+        User lyes = readerFactory.createUser(1, "aziz" , "kimo@gmail.com");
+        System.out.println(akram.getName()+ "and , " + lyes.getEmail());
     }
 }
--- src/main/java/myLibrary/User.java
+++ src/main/java/myLibrary/User.java
@@ -2,35 +2,63 @@ package main.java.myLibrary;
 
 // factory method pattern to manage users
 
-public interface User {    
-       
-}
+public abstract class User {
+    private int id;
+    private String name;
+    private String email;
 
-class Admin implements User {
+    public User( int id, String name, String email) {
+        this.id = id; 
+        this.name =  name;
+        this.email = email;
+    }
 
-}
+    public int getId() {
+        return id;
+    }
 
-class Reader implements User {
+    public String getName() {
+        return name;
+    }
     
+    public String getEmail() {
+        return email;
+    }
+}
+
+class Admin extends User {
+    private boolean isAdmin;
+    public Admin(int id, String name, String email) {
+        super(id, name, email);
+        this.isAdmin = true;
+    }
+}
+
+class Reader extends User {
+    private boolean isAdmin;
+    public Reader(int id, String name, String email) {
+        super(id, name, email);
+        this.isAdmin = false;
+    }
 }
 
 // Creator
 abstract class UserCreator {
-    abstract User createUser();
+    abstract User createUser(int id, String name, String email);
 }
 
 // ConcreteCreators 
 class AdminCreator extends UserCreator {
     @Override
-    User createUser() {
-        return new Admin();
+    User createUser(int id, String name, String email) {
+        return new Admin(id, name, email);
     }
 }
 
 class ReaderCreator extends UserCreator {
     @Override
-    User createUser() {
-        return new Reader();
+    User createUser(int id, String name, String email) {
+        return new Reader(id, name, email);
     }
 }
 
